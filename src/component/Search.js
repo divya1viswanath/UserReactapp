6b74ff9d0c3de2363fa47dc4d574779181319f81
divya1viswanath <divyarugmini798@gmail.com>
2022-05-18 07:43:19 +0530

    Search

---
+++ src/component/Search.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+
+function Search() {
+  return (
+    <div>
+        <Header/>
+        Search
+
+    </div>
+  )
+}
+
+export default Search
