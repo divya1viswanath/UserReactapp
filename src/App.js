73390b873f4bcfc477d99795b08ff2215abd1fae
divya1viswanath <divyarugmini798@gmail.com>
2022-05-18 06:49:22 +0530

    AddUser created

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.rtl.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Adduser from './component/Adduser';
 
 function App() {
   return (
     <>
-      UserApp
+      <Adduser/>
     </>
   );
 }
