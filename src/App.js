2f917ef406b3131e2273492258f0c65c8dccd82c
divya1viswanath <divyarugmini798@gmail.com>
2022-05-18 07:37:43 +0530

    Navabar completed

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.rtl.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Adduser from './component/Adduser';
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <Adduser/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/'exact element={<Adduser/>}/>
+        </Routes>                                                                                   
+      </BrowserRouter>
     </>
   );
 }
