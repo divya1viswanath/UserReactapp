7068dcc6c3272c7e8d0a678cafb85d1b282030d6
divya1viswanath <divyarugmini798@gmail.com>
2022-05-18 07:56:08 +0530

    viewuser

---
+++ src/component/Viewuser.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+
+const Viewuser = () => {
+  return (
+    <div>
+        <Header/>
+        Viewuser
+
+    </div>
+  )
+}
+
+export default Viewuser
